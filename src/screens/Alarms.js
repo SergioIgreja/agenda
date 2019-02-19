f56b536d67b2e11bb02ea3f68384ae5c4db49358
Sérgio Igreja <sergioeduardoigreja16@gmail.com>
2019-02-19 13:12:50 +0000

    Clock/timer is now functional

--- src/screens/Alarms.js
+++ src/screens/Alarms.js
@@ -5,22 +5,97 @@ import {
     TouchableOpacity,
     View,
     StyleSheet,
-    Alert,
+    Vibration,
 } from 'react-native';
 
 export default class AlarmsScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            seconds: 10,
+            countDown: false,
+            interval: null,
+            percentage: 0,
+        };
+    }
+
+    reset() {
+        this.setState({
+            seconds: 10,
+            countDown: false,
+            interval: null,
+            percentage: 0
+        });
+    }
+
+    stopTimer() {
+        clearInterval(this.state.interval);
+
+        this.setState(prevState => {
+            return {
+                seconds: prevState.seconds,
+                interval: null,
+                countDown: false,
+                percentage: prevState.percentage
+            }
+        })
+    }
+
+    calculatePercentage() {
+        const percentageIncrease = 10;
+        this.setState(prevState => {
+            return {
+                percentage: prevState.percentage + percentageIncrease
+            }
+        });
+    }
+
+    tick() {
+        this.setState({
+            seconds: this.state.seconds - 1
+        })
+        this.calculatePercentage()
+        if(this.state.seconds === 0) {
+            this.stopTimer();
+            Vibration.vibrate();
+            this.reset();
+        }
+    }
+
+    startTimer() {
+        let interval = setInterval(() => {
+            if((this.state.seconds > 0 ) && this.state.countDown) {
+                this.tick()
+            }
+        }, 1000);
+
+        this.setState(prevState => {
+            return {
+                seconds: prevState.seconds,
+                countDown: true,
+                interval: interval
+            };
+        });
+    }
+
+    secondsToMinutes() {
+        const minutes = Math.floor(this.state.seconds / 60);
+        const seconds = this.state.seconds % 60;
+
+        return `${minutes}:${seconds}`;
+    }
 
     render() {
         return (
             <View style={styles.container}>
-                <PercentageCircle radius={150} bgcolor='#000' color='#f39c12' borderWidth={20} percent={40} padding={10}>
-                    <Text style={styles.time}>25:00</Text>
+                <PercentageCircle radius={150} bgcolor='#000' color='#f39c12' borderWidth={20} percent={this.state.percentage} padding={10}>
+                    <Text style={styles.time}>{this.secondsToMinutes()}</Text>
                 </PercentageCircle>
                 <View style={styles.buttonsContainer}>
-                    <TouchableOpacity style={styles.button} onPress={() => Alert.alert('start')}>
+                    <TouchableOpacity style={this.state.countDown ? styles.buttonDisabled : styles.button} onPress={() => this.startTimer()} disabled={this.state.countDown}>
                         <Text style={styles.text}>Start</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={() => Alert.alert('stop')}>
+                    <TouchableOpacity style={this.state.countDown ? styles.button : styles.buttonDisabled} onPress={() => this.stopTimer()} disabled={!this.state.countDown}>
                         <Text style={styles.text}>Stop</Text>
                     </TouchableOpacity>
                 </View>
@@ -55,6 +130,17 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginLeft: 10,
         marginRight: 10,
+        opacity: 1.0
+    },
+    buttonDisabled: {
+        backgroundColor: '#f39c12',
+        width: '30%',
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+        opacity: 0.3
     },
     text: {
         fontSize: 25,
